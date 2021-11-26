60510e2101c063ee106c0c1c579945ab9f79fb3d
EngrAnasSheikh <anas_mudassar@yahoo.co.uk>
2021-11-26 23:58:04 +0000

    Products Class

--- BusinessRulesApp/Program.cs
+++ BusinessRulesApp/Program.cs
@@ -1,7 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
+/*
+ * Developer Anas Mudassar
+ * Version 1.0
+ * Business Rules Engine
+ */
 namespace BusinessRulesApp
 {
+    //Class for Products
+    public abstract class Products
+    {
+        //List Array for Physical Products
+        public List<string> PhysicalProduct { get; set; }
+
+        //List Array for Non Physical Products
+        public List<string> NonPhysicalProduct { get; set; }
+
+        //List Array for All Instructions
+        public List<string> Instructions { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
