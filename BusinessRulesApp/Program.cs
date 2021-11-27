06686f9e02eaf6c6b3bd8206c267f05dde4b7556
EngrAnasSheikh <anas_mudassar@yahoo.co.uk>
2021-11-27 00:10:53 +0000

    Physical Product Class

--- BusinessRulesApp/Program.cs
+++ BusinessRulesApp/Program.cs
@@ -20,6 +20,48 @@ namespace BusinessRulesApp
         //List Array for All Instructions
         public List<string> Instructions { get; set; }
     }
+
+    //Class for Physical Products
+    class PhysicalProducts : Products
+    {
+        //function for shipping packing slip
+        public void ShipSlip()
+        {
+            Instructions.Add("Packing Slip for Shipping Generated");
+            Console.WriteLine(Instructions[0]);
+        }
+
+        //Function for Royality Slip
+        public void RoyaltySlip()
+        {
+            Instructions.Add("Duplicate Packing Slip for Royalty Department Generated");
+            Console.WriteLine(Instructions[1]);
+        }
+
+        //function for Commission Slip
+        public void CommissionSlip()
+        {
+            Instructions.Add("Commission Payment to the agent Generated.");
+            Console.WriteLine(Instructions[2]);
+        }
+
+        //Funtion for Book
+        public void Book(string BookName)
+        {
+            PhysicalProduct.Add(BookName);
+            ShipSlip();
+            RoyaltySlip();
+            CommissionSlip();
+        }
+
+        //Funtion for Any Other Physical Product
+        public void OtherPhyscialProduct(string Other)
+        {
+            PhysicalProduct.Add(Other);
+            ShipSlip();
+            CommissionSlip();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
