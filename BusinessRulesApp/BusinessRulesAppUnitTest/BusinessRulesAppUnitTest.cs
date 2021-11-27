488924ce0ce4cacf0d3d23446e13182921db691d
EngrAnasSheikh <anas_mudassar@yahoo.co.uk>
2021-11-27 23:53:48 +0000

    Unit Test Started

---
+++ BusinessRulesApp/BusinessRulesAppUnitTest/BusinessRulesAppUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessRulesApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessRulesApp.BusinessRulesAppUnitTest
+{
+    //Test Class for BusinessRulesApp
+    [TestClass]
+    public class BusinessRulesAppUnitTest
+    {
+
+    }
+}
