77af72f6b9a999c354c1c555b60d4e893bd03de0
EngrAnasSheikh <anas_mudassar@yahoo.co.uk>
2021-11-28 22:53:41 +0000

    Final Changes

--- BusinessAppUnitTestNew/UnitTesting.cs
+++ BusinessAppUnitTestNew/UnitTesting.cs
@@ -1,6 +1,13 @@
 using BusinessRulesApp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
+/*
+ * Developer Anas Mudassar
+ * Unit Test Version 1.0
+ * Business Rules Engine Unit Testing
+ */
+
 namespace BusinessAppUnitTestNew
 {
     [TestClass]
@@ -10,21 +17,21 @@ namespace BusinessAppUnitTestNew
         [TestMethod]
         public void BookTest()
         {
-            var check = new Book("Programming");
-            Assert.AreEqual("Programming", check.PhyProductName);
-            Assert.AreEqual("Packing Slip for Shipping Generated.", check.PhyInstructions[0]);
-            Assert.AreEqual("Duplicate Packing Slip for Royalty Department Generated.", check.PhyInstructions[1]);
-            Assert.AreEqual("Commission Payment to the agent Generated.", check.PhyInstructions[2]);
+            var check = new Book("Programming"); //Enter name of the Book
+            Assert.AreEqual("Programming", check.PhyProductName); //Test the Name of the Book
+            Assert.AreEqual("Packing Slip for Shipping Generated.", check.PhyInstructions[0]); //Test Shipping Slip
+            Assert.AreEqual("Duplicate Packing Slip for Royalty Department Generated.", check.PhyInstructions[1]); //Test Slip for Royalty Department
+            Assert.AreEqual("Commission Payment to the agent Generated.", check.PhyInstructions[2]); //Test Slip for Agent
         }
 
         //Unit Testing for OtherPhysicalProducts Class
         [TestMethod]
         public void OtherPhysicalProductsTest()
         {
-            var check = new OtherPhysicalProduct("Bat");
-            Assert.AreEqual("Bat", check.PhyProductName);
-            Assert.AreEqual("Packing Slip for Shipping Generated.", check.PhyInstructions[0]);
-            Assert.AreEqual("Commission Payment to the agent Generated.", check.PhyInstructions[2]);
+            var check = new OtherPhysicalProduct("Bat"); //Enter name of the Other Physical Product
+            Assert.AreEqual("Bat", check.PhyProductName); //Test the Name of the Other Item
+            Assert.AreEqual("Packing Slip for Shipping Generated.", check.PhyInstructions[0]); //Test the Shipping Slip
+            Assert.AreEqual("Commission Payment to the agent Generated.", check.PhyInstructions[2]); //Test Slip for Royalty Department
         }
 
         //Unit Testing for Membership Class
@@ -32,8 +39,8 @@ namespace BusinessAppUnitTestNew
         public void MembershipTest()
         {
             var check = new Membership();
-            Assert.AreEqual("Membership Activated.", check.NonPhyInstructions[0]);
-            Assert.AreEqual("Email Sent to Owner.", check.NonPhyInstructions[2]);
+            Assert.AreEqual("Membership Activated.", check.NonPhyInstructions[0]); //Test Membership Message
+            Assert.AreEqual("Email Sent to Owner.", check.NonPhyInstructions[2]); //Test email Sent
         }
 
         //Unit Testing for Upgrade Membership Class
@@ -41,26 +48,26 @@ namespace BusinessAppUnitTestNew
         public void UpgradeMembershipTest()
         {
             var check = new UpgradeMembership();
-            Assert.AreEqual("Membership Upgraded.", check.NonPhyInstructions[1]);
-            Assert.AreEqual("Email Sent to Owner.", check.NonPhyInstructions[2]);
+            Assert.AreEqual("Membership Upgraded.", check.NonPhyInstructions[1]); //Test Membership Upgrade
+            Assert.AreEqual("Email Sent to Owner.", check.NonPhyInstructions[2]); //Test Email Sent
         }
 
         //Unit Testing for Video Class Condition else
         [TestMethod]
         public void VideoTest1()
         {
-            var check = new Video("SuperMan");
-            Assert.AreEqual("SuperMan", check.NonPhyProductName);
-            Assert.AreEqual("You have added a Video", check.NonPhyInstructions[4]);
+            var check = new Video("SuperMan"); //Enter name of the Video
+            Assert.AreEqual("SuperMan", check.NonPhyProductName); //Test the name of the Video
+            Assert.AreEqual("You have added a Video", check.NonPhyInstructions[4]); //Test the message for Other Video
         }
 
         //Unit Testing for Video Class Condition 'learning to ski"
         [TestMethod]
         public void VideoTest2()
         {
-            var check = new Video("learning to ski");
-            Assert.AreEqual("learning to ski", check.NonPhyProductName);
-            Assert.AreEqual("'First Aid' Video has been added.", check.NonPhyInstructions[3]);
+            var check = new Video("learning to ski"); //Enter name of the Video
+            Assert.AreEqual("learning to ski", check.NonPhyProductName); //Test the name of the Video
+            Assert.AreEqual("'First Aid' Video has been added.", check.NonPhyInstructions[3]); //Test the 'First Aid' Video Add
         }
     }
 }
--- BusinessRulesApp/BusinessRulesApp/Program.cs
+++ BusinessRulesApp/BusinessRulesApp/Program.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
  */
 namespace BusinessRulesApp
 {
-    class Program
+    public class Program
     {
         static void Main()
         {
